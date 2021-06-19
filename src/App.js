76c4748b1758c4d60145a7fd563ad7c890998d3e
Nhung Hang <hangbichnhung@gmail.com>
2021-06-20 02:04:12 +0700

    create file upload component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-// import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { Collage } from "./CollageComponent";
+import FileUpload from "./FileUpload/file-upload.component";
+// import { Collage } from "./CollageComponent";
 
 function App() {
-  return <Collage />;
+  // return <Collage />;
+  const [newCollection, setNewCollection] = useState({
+    collectionImages: [],
+  });
+
+  const upadteUploadedFiles = (files) => {
+    setNewCollection({ ...newCollection, collectionImages: files });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <FileUpload
+          accept=".jpg,.png,jpeg"
+          label="Collection Image(s)"
+          multiple
+          updateFilesCb={upadteUploadedFiles}
+        ></FileUpload>
+        <button type="submit">Create New Collection</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/FileUpload/file-upload.component.jsx
@@ -0,0 +1,158 @@
+import React, { useRef, useState } from "react";
+import {
+  FileUploadContainer,
+  FormField,
+  DragDropText,
+  UploadFileBtn,
+  FilePreviewContainer,
+  ImagePreview,
+  PreviewContainer,
+  PreviewList,
+  FileMetaData,
+  RemoveFileIcon,
+  InputLabel,
+} from "./file-upload.styles";
+
+const DEFAULT_MAX_FILE_SIZE_IN_BYTES = 5000000;
+const KILO_BYTES_PER_BYTE = 1000;
+
+const convertBytesToKb = (bytes) => Math.round(bytes / KILO_BYTES_PER_BYTE);
+
+const FileUpload = ({
+  label,
+  updateFilesCb,
+  maxFileSizeInBytes = DEFAULT_MAX_FILE_SIZE_IN_BYTES,
+  ...otherProps
+}) => {
+  const fileInputField = useRef(null);
+  const [files, setFiles] = useState({});
+
+  const handleUploadBtnClick = () => {
+    fileInputField.current.click();
+  };
+
+  const convertNestedObjectArray = (nestedObj) =>
+    Object.keys(nestedObj).map((key) => nestedObj[key]);
+
+  const callUpdateFileCb = (files) => {
+    const filesAsArray = convertNestedObjectArray(files);
+    updateFilesCb(filesAsArray);
+  };
+
+  // returns an object where the key is the file name and the value is the File object
+  const addNewFiles = (newFiles) => {
+    for (let file of newFiles) {
+      if (file.size <= maxFileSizeInBytes) {
+        if (!otherProps.multiple) {
+          return { file };
+        }
+        files[file.name] = file;
+      }
+    }
+    return { ...files };
+  };
+
+  const handleNewFileUpload = (e) => {
+    const { files: newFiles } = e.target;
+    if (newFiles.length) {
+      let updatedFiles = addNewFiles(newFiles);
+      setFiles(updatedFiles);
+
+      // any changes to the files state must be sent to the parent component
+      callUpdateFileCb(updateFilesCb);
+    }
+  };
+
+  const removeFile = (fileName) => {
+    delete files[fileName];
+    setFiles({ ...files });
+    callUpdateFileCb({ ...files });
+  };
+
+  // drap and drop
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const { files: newFiles } = e.dataTransfer;
+    if (newFiles.length) {
+      let updatedFiles = addNewFiles(newFiles);
+      setFiles(updatedFiles);
+
+      // any changes to the files state must be sent to the parent component
+      callUpdateFileCb(updateFilesCb);
+    }
+  };
+
+  return (
+    <>
+      <FileUploadContainer
+        onDrop={(e) => handleDrop(e)}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
+        <InputLabel>{label}</InputLabel>
+        <DragDropText>Drag and drop your files anywhere or</DragDropText>
+        <UploadFileBtn type="button" onClick={handleUploadBtnClick}>
+          <i className="fas fa-file-upload"></i>
+          <span>Upload {otherProps.multiple ? "files" : "a file"}</span>
+        </UploadFileBtn>
+        <FormField
+          type="file"
+          ref={fileInputField}
+          onChange={handleNewFileUpload}
+          title=""
+          value=""
+          {...otherProps}
+        />
+      </FileUploadContainer>
+
+      {/*second part starts here*/}
+      <FilePreviewContainer>
+        <span>To Upload</span>
+        <PreviewList>
+          {Object.keys(files).map((fileName, index) => {
+            let file = files[fileName];
+            let isImageFile = file.type.split("/")[0] === "image";
+            return (
+              <PreviewContainer key={fileName}>
+                <div>
+                  {isImageFile && (
+                    <ImagePreview
+                      src={URL.createObjectURL(file)}
+                      alt={`file preview ${index}`}
+                    />
+                  )}
+                  <FileMetaData isImageFile={isImageFile}>
+                    <span>{file.name}</span>
+                    <aside>
+                      <span>{convertBytesToKb(file.size)} kb</span>
+                      <RemoveFileIcon
+                        className="fas fa-trash-alt"
+                        onClick={() => removeFile(fileName)}
+                      ></RemoveFileIcon>
+                    </aside>
+                  </FileMetaData>
+                </div>
+              </PreviewContainer>
+            );
+          })}
+        </PreviewList>
+      </FilePreviewContainer>
+    </>
+  );
+};
+
+export default FileUpload;
